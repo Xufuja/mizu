eac3c45cb0d0060c8cfcd1886f726c4d4e0e5956
Xufuja <xufuja@gmail.com>
2024-06-28 23:20:59 +0200

    Read file and print contents

--- src/main/java/dev/xfj/Main.java
+++ src/main/java/dev/xfj/Main.java
@@ -1,7 +1,20 @@
 package dev.xfj;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if (args.length != 1) {
+            System.err.println("Incorrect usage!");
+            System.err.println("Pass a .mz file!");
+            System.exit(1);
+        }
+        try {
+            String input = Files.readString(Path.of(args[0]));
+            System.out.println(input);
+        }catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 }
